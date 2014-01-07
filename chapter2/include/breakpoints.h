d0376abbc7443dfa3ff1565013a5fa99a921a228
Aleh Suprunovich <br@ahlamon.org>
2014-01-07 21:16:07 +0300

    indent applied

--- chapter2/include/breakpoints.h
+++ chapter2/include/breakpoints.h
@@ -4,9 +4,9 @@
 #define LINELENGTH (80)
 
 typedef struct breakpoint {
-                double time;
-                double value;
+	double time;
+	double value;
 } BREAKPOINT;
 
-BREAKPOINT maxpoint(const BREAKPOINT* points,long npoints);
-BREAKPOINT* get_breakpoints(FILE* fp, long* psize);
+BREAKPOINT maxpoint(const BREAKPOINT * points, long npoints);
+BREAKPOINT *get_breakpoints(FILE * fp, long *psize);
